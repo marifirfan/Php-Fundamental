25eacf02617d45bf0e9461b93b69b9b93f486f7e
Muhammad Arif Irfan <marifirfannn@gmail.com>
2024-03-12 20:41:35 +0700

    update latihan 4

--- Latihan4(Function)/index.php
+++ Latihan4(Function)/index.php
@@ -3,5 +3,14 @@
 // echo ("<br>");
 // echo time();
 // echo ("<br>");
-echo date("l" , time() + 60 * 60 * 24 * 5 );
+echo date("l, m - M - Y" , time() + 60 * 60 * 24 * 5 );
+echo ("<br>");
+echo date("l , m - M - Y ", time() - 60 * 60 * 24 * 365 * 20);
+echo ("<br>");
+echo date("l", mktime(0,0,0, 7, 9, 2003));
+echo ("<br>");
+echo date("l, m M Y", strtotime("25 jul 2003"));
+echo ("<br>");
+var_dump(strlen("aku adalah superman")); 
+
 ?>
