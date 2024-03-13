93551ccba3502b68a8d8e28f2d990d5c559f9513
Muhammad Arif Irfan <marifirfannn@gmail.com>
2024-03-13 13:44:00 +0700

    update latihan 5

---
+++ Latihan5(Array)/Latihan1.php
@@ -0,0 +1,11 @@
+<?php  
+// array
+// variabel mempunyai banyak nilai
+$nama = ["Irfan", "Rijal"];
+$index = 0;
+// print_r($nama);
+while ($index < count($nama)) {
+    echo $nama[$index]."\n"; 
+    $index++;
+}
+?>
