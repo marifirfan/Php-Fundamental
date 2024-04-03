e218e15c3272909a5fd21c540161199bc6f3ea78
Muhammad Arif Irfan <marifirfannn@gmail.com>
2024-04-03 17:07:23 +0700

    update latihan 7

---
+++ Latihan7(GetPost)/welcomeadnim.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>welcome</title>
+    </head>
+    <body>
+        <h1>Selamat Datang Admin</h1>
+
+        <form action="admin.php" method="post" >
+        <button type="submit" name="logout" >logout</button>
+        </form>
+        
+        
+    </body>
+</html>
